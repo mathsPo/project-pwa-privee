e5d05c89afbfa05dde00e33b13cc80fb30a31cc4
Mathis Pol <mathis.pol@etudiant.univ-reims.fr>
2022-05-20 15:16:57 +0200

    Changement spinner et deleteModel pour qu'il soit fonctionel

--- js/ihm.js
+++ js/ihm.js
@@ -20,17 +20,18 @@ const mv = document.querySelector("div.model-viewer")
  * Ajout d'un spinner au chargement de la page
  */
 function startSpinner(){
-    spinner.classList.remove('hidden');
+    // spinner.classList.remove('hidden');
+    console.log("spinner non fonctionel");
 }
 
 /**
  * Suppression du spinner
  */
 function stopSpinner(){
-    window.setTimeout(function(){
+    /*window.setTimeout(function(){
         spinner.classList.add('hidden');
-    }, 300)
-
+    }, 300) */
+    console.log("spinner non fonctionel");
 }
 
 /** 
